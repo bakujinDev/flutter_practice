4ad50931ef6a08ae75fca6526aae9a3201adda37
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-03-18 22:03:17 +0900

    pratice provider

---
+++ lib/features/provider/level1.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/main.dart';
+import 'package:provider/provider.dart';
+
+class Level1 extends StatelessWidget {
+  const Level1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('You have pushed the button this many times:'),
+            Count(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('increment_floatingActionButton'),
+
+        /// Calls `context.read` instead of `context.watch` so that it does not rebuild
+        /// when [Counter] changes.
+        onPressed: () => context.read<Counter>().increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Count extends StatelessWidget {
+  const Count({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      /// Calls `context.watch` to make [Count] rebuild when [Counter] changes.
+      '${context.watch<Counter>().count}',
+      key: const Key('counterState'),
+      style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/features/provider/level1.dart';
 import 'package:flutter_practice/features/todoey/screens/task_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,30 +20,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      // theme: ThemeData.dark().copyWith(
-      //   primaryColor: const Color(0xff0a0e21),
-      //   scaffoldBackgroundColor: const Color(0xff0a0e21),
-      //   appBarTheme: AppBarTheme.of(context).copyWith(
-      //     backgroundColor: const Color(0xff0a0e21),
-      //   ),
-      //   textTheme: const TextTheme(
-      //     titleMedium: TextStyle(
-      //       color: Colors.black,
-      //     ),
-      //     bodyLarge: TextStyle(
-      //       color: Colors.black,
-      //     ),
-      //     bodyMedium: TextStyle(
-      //       color: Colors.black,
-      //     ),
-      //     bodySmall: TextStyle(
-      //       color: Colors.black,
-      //     ),
-      //   ),
-      // ),
-      home: TaskScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Counter()),
+      ],
+      child: const MaterialApp(
+        title: 'Flutter Demo',
+        // theme: ThemeData.dark().copyWith(
+        //   primaryColor: const Color(0xff0a0e21),
+        //   scaffoldBackgroundColor: const Color(0xff0a0e21),
+        //   appBarTheme: AppBarTheme.of(context).copyWith(
+        //     backgroundColor: const Color(0xff0a0e21),
+        //   ),
+        //   textTheme: const TextTheme(
+        //     titleMedium: TextStyle(
+        //       color: Colors.black,
+        //     ),
+        //     bodyLarge: TextStyle(
+        //       color: Colors.black,
+        //     ),
+        //     bodyMedium: TextStyle(
+        //       color: Colors.black,
+        //     ),
+        //     bodySmall: TextStyle(
+        //       color: Colors.black,
+        //     ),
+        //   ),
+        // ),
+        home: Level1(),
+      ),
     );
+  }
+}
+
+class Counter extends ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
   }
 }
